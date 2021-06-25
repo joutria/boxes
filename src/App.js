b8423240c5d055ceb1b38ff901787b81c5d5e9c7
joutria <joutria@gmail.com>
2021-06-24 19:52:48 -0500

    minor changes to shadows

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Hobbies from './Hobbies';
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className="title">
         <h1>Boxes</h1>
       </div>
       <div className="container">
